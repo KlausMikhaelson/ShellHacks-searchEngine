077e226421f3643cd0b06935a2394c8539dce6ac
KlausMikhaelson <satyamsingh5076@gmail.com>
2022-09-10 16:10:06 +0530

    fixed fE bug

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,20 @@
 import './App.css';
 import SerchInput from './component/SerchInput';
 import Navbar from './component/Navbar';
+import {Routes, Route, Link} from "react-router-dom"
+import Result from './pages/Result';
+import HomePage from './pages/HomePage';
 
-function App() {
+const App = () => {
   return (
-    <>
-    <Navbar />
-    <SerchInput />    
-    </>
+    <div className='App'>
+      <Navbar />
+      <div id='page-body'></div>
+      <Routes>
+        <Route path='/' element={<HomePage />} exact />
+        <Route path='/result' element={<Result />} />
+      </Routes>
+    </div>
   );
 }
 
---
+++ frontend/src/pages/HomePage.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from '../component/Navbar'
+import SerchInput from '../component/SerchInput'
+
+const HomePage = () => {
+  return (
+    <>
+    <SerchInput />
+    </>
+  )
+}
+
+export default HomePage
