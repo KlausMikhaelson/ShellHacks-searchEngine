e7e685296a1fd130a55d9626a316109c0a9ec4b3
KlausMikhaelson <satyamsingh5076@gmail.com>
2022-09-10 11:07:54 +0530

    added dark background and styled search input

--- frontend/src/component/SerchInput.js
+++ frontend/src/component/SerchInput.js
@@ -1,11 +1,17 @@
 import React from 'react'
 import "./SearchInput.css"
+import search from "../assets/search.png"
 
 const SerchInput = () => {
   return (
     <>
     <div className='Search'>
-    <input className='search-input' type="text" />
+    <label className='Search-inbox'>
+        <span className='search-input-inside'>
+            <input className='search-input' type="text" />
+        </span>
+    <img className='search-icon' src={search} alt="search icon"/>
+    </label>
     </div>
     </>
   )
