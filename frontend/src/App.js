c7a46594db1e3e409b0acf5ba4628d3e0ac0bbb7
KlausMikhaelson <satyamsingh5076@gmail.com>
2022-09-10 11:55:52 +0530

    added search button

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import SerchInput from './component/SerchInput';
+import Navbar from './component/Navbar';
 
 function App() {
   return (
     <>
+    <Navbar />
     <SerchInput />    
     </>
   );
--- frontend/src/component/SerchInput.js
+++ frontend/src/component/SerchInput.js
@@ -3,6 +3,9 @@ import "./SearchInput.css"
 import search from "../assets/search.png"
 
 const SerchInput = () => {
+    const myFunction = () => {
+        console.log("Clicked")
+    }
   return (
     <>
     <div className='Search'>
@@ -10,7 +13,9 @@ const SerchInput = () => {
         <span className='search-input-inside'>
             <input className='search-input' type="text" />
         </span>
+        
     <img className='search-icon' src={search} alt="search icon"/>
+    <button type='submit' className='search-btn' onClick={myFunction}></button>
     </label>
     </div>
     </>
