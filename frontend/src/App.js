06e4b42866f40c0faceb1fd33c7df48a933afa4a
KlausMikhaelson <satyamsingh5076@gmail.com>
2022-09-10 10:12:44 +0530

    initial setup

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import SerchInput from './component/SerchInput';
+
+function App() {
+  return (
+    <>
+    <SerchInput />    
+    </>
+  );
+}
+
+export default App;
---
+++ frontend/src/component/SerchInput.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./SearchInput.css"
+
+const SerchInput = () => {
+  return (
+    <>
+    <div className='Search'>
+    <input className='search-input' type="text" />
+    </div>
+    </>
+  )
+}
+
+export default SerchInput
